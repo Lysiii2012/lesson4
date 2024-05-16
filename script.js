ce127c4375df696884a3b1c2e4710d666c8b4b4c
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-05-16 19:59:12 +0300

    secons

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const dollarCourse = 26;
-const dollars = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+let userNumber = prompt("Введіть ціле число:");
+let number = parseInt(userNumber);
 
-for (let i = 0; i < dollars.length; i++){
-    const amountDollars = dollars[i];
-    const amountHryvnia = amountDollars * dollarCourse;
-    console.log(`${amountDollars} доларів коштують ${amountHryvnia} гривень`)
+function whathNum(number) {
+    if (number <= 1) {
+        return false;
+    }
+    let divisor = 2;
+    while (divisor <= Math.sqrt(number)) {
+        if (number % divisor === 0) {
+            return false; 
+        }
+        divisor++;
+    }
+    return true; 
+}
+
+if (isNaN(number)) {
+    console.log("Ви ввели не число.");
+} else {
+    console.log(whathNum(number) ? `${number} - просте число.` : `${number} - не є простим числом.`);
 }
