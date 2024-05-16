a1abcde8a2618c52c8332bac7d03ca0f94eb7157
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-05-16 21:19:22 +0300

    com

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-let userNumber = prompt("Введіть ціле число:");
-let number = parseInt(userNumber);
+const userNum = parseInt(prompt('Введіть ціле число'))
 
-function whathNum(number) {
-    if (number <= 1) {
-        return false;
-    }
-    let divisor = 2;
-    while (divisor <= Math.sqrt(number)) {
-        if (number % divisor === 0) {
-            return false; 
-        }
-        divisor++;
-    }
-    return true; 
-}
-
-if (isNaN(number)) {
-    console.log("Ви ввели не число.");
+if (isNaN(userNum) || userNum <= 0) {
+    console.log("Ви ввели некоректне число.");
 } else {
-    console.log(whathNum(number) ? `${number} - просте число.` : `${number} - не є простим числом.`);
+    let number = 1;
+    while (number * number <= userNum && number <= 100) {
+        console.log(number);
+        number++;
+    }
 }
